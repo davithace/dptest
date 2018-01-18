927ffd84b326f7ba836f52185c238ccf0895d1e9
davithace <davidpras1994@gmail.com>
2018-01-18 09:56:12 +0700

    Updater

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,6 +1,37 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/browser/notification.js",
+        "id": "cordova-plugin-dialogs.notification_browser",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/src/browser/InAppBrowserProxy.js",
+        "id": "cordova-plugin-inappbrowser.InAppBrowserProxy",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "runs": true
+    },
+    {
         "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
         "id": "cordova-plugin-statusbar.statusbar",
         "pluginId": "cordova-plugin-statusbar",
@@ -31,32 +62,17 @@ module.exports = [
         "clobbers": [
             "PushNotification"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
-        "id": "cordova-plugin-dialogs.notification",
-        "pluginId": "cordova-plugin-dialogs",
-        "merges": [
-            "navigator.notification"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-dialogs/www/browser/notification.js",
-        "id": "cordova-plugin-dialogs.notification_browser",
-        "pluginId": "cordova-plugin-dialogs",
-        "merges": [
-            "navigator.notification"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-statusbar": "2.1.3",
-    "phonegap-plugin-push": "1.8.4",
-    "cordova-plugin-dialogs": "1.2.1",
     "cordova-plugin-console": "1.0.7",
-    "cordova-plugin-whitelist": "1.2.2"
+    "cordova-plugin-dialogs": "1.2.1",
+    "cordova-plugin-inappbrowser": "1.7.2",
+    "cordova-plugin-statusbar": "2.1.3",
+    "cordova-plugin-whitelist": "1.2.2",
+    "phonegap-plugin-push": "1.8.4"
 }
 // BOTTOM OF METADATA
 });
--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,125 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+
+ /*
+ 
+	APPLICATION
+ 
+ */
+ 
+var ref;
+
+var mainDomain  	= "gietimes.com";
+var mainHomeUrl 	= "https://gietimes.com/";  //with backslash
+var is_inappbrowser = false;
+ 
+var inappbrowserStartCallback = 
+	function(event) { 
+		
+		navigator.notification.activityStart("", "Loading . . . ");
+		setTimeout(function(){ 
+			navigator.notification.activityStop() 
+			}, 20000);
+		
+		//Whitelist to system browser
+		var currenturl = event.url;
+		var waa = /api.whatsapp.com/;
+		var sms = /sms:/;
+		//redirect if
+		if(currenturl.match(waa) || currenturl.match(sms)){
+			var ref = cordova.InAppBrowser.open( currenturl, '_system');
+			history.go(-1);
+			navigator.app.backHistory();
+		}
+		
+	}
+	
+var inappbrowserStopCallback = 
+	function(event) {
+		navigator.notification.activityStop();
+		//SpinnerPlugin.activityStop();
+		ref.show();
+	}
+	
+var inappbrowserExitCallback = 
+	function(event) {
+		setTimeout(function(){ 
+			exitConfirm();
+		}, 1000);
+	}
+	
+var inappbrowserErrorCallBack = 
+	function(event) {
+		navigator.notification.confirm("Kesalahan koneksi server. Anda ingin keluar dari aplikasi ?", onConfirm, "Confirmation", "Ya,Tidak"); 
+	}	
+
+	
+function exitConfirm(){
+	navigator.notification.confirm("Anda ingin keluar dari aplikasi ?", onConfirm, "Confirmation", "Ya,Tidak");
+}
+	
+function callbackBridge(param) {
+    alert(param);
+}
+
+function openBrowserListenerInit(starturl){
+	var ref = cordova.InAppBrowser.open( starturl, '_self', 'location=no, toolbar=no, EnableViewPortScale=yes, zoom=no, hidden=yes');
+	ref.addEventListener('loadstart', inappbrowserStartCallback);
+	ref.addEventListener('loadstop', inappbrowserStopCallback);
+	ref.addEventListener('exit', inappbrowserExitCallback);
+	ref.addEventListener('loaderror', inappbrowserErrorCallBack);
+}
+
+function goToHome() {
+    //var starturl = mainHomeUrl;
+	var starturl = "https://gietimes.com/apps/index.php"
+	if(is_inappbrowser){
+		openBrowserListenerInit(starturl);
+	}else{
+		if($("#maincontent").attr('src')==""){
+			$("#maincontent").attr('src', starturl);
+		}else{
+			return false;
+		}
+		
+	}
+}
+
+function goToUrl(starturl) {
+
+	if(is_inappbrowser){
+		openBrowserListenerInit(starturl);
+	}else{
+		//open iframe
+		$("#maincontent").attr('src', starturl);
+	}
+		
+}		
+
+function onBackKeyDown(e) {
+    e.preventDefault();
+    exitConfirm(); 
+    // Prompt the user with the choice
+}
+
+function onConfirm(button) {
+    if(button==2){//If User selected No, then we just do nothing
+        if(is_inappbrowser){
+			goToHome();
+		}else{
+			return false;
+		}
+    }else{
+        navigator.app.exitApp();// Otherwise we quit the app.
+    }
+}
+
+function register(){
+	app.setupPush();
+}
+ 
+ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
@@ -27,6 +145,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+		document.addEventListener("backbutton", onBackKeyDown, false); 
     },
     // deviceready Event Handler
     //
@@ -35,13 +154,25 @@ var app = {
     onDeviceReady: function() {
         console.log('Received Device Ready Event');
         console.log('calling setup push');
-        app.setupPush();
+		register();
+		//setTimeout(function(){ register(); }, 5000);
+		
+		
+		var connectionStatus = false;
+		connectionStatus = navigator.onLine ? 'online' : 'offline';
+		if(connectionStatus=="offline"){
+			navigator.notification.confirm("Tidak ada koneksi Internet. Anda ingin keluar dari aplikasi ?", onConfirm, "Confirmation", "Ya,Tidak"); 
+		}else{
+			goToHome();
+		}
+			
+		
     },
     setupPush: function() {
         console.log('calling push init');
         var push = PushNotification.init({
             "android": {
-                "senderID": "XXXXXXXX"
+                "senderID": "439977014115"
             },
             "browser": {},
             "ios": {
@@ -63,7 +194,10 @@ var app = {
                 // Post registrationId to your app server as the value has changed
             }
 			
-			alert(data.registrationId);
+			var theUrl = mainHomeUrl+"android/pusher/initdevice.php?deviceid="+data.registrationId;
+			var xmlHttp = new XMLHttpRequest();
+			xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+			xmlHttp.send( null );
 
             var parentElement = document.getElementById('registration');
             var listeningElement = parentElement.querySelector('.waiting');
@@ -71,6 +205,9 @@ var app = {
 
             listeningElement.setAttribute('style', 'display:none;');
             receivedElement.setAttribute('style', 'display:block;');
+			
+			//alert("registered");
+
         });
 
         push.on('error', function(e) {
@@ -79,12 +216,36 @@ var app = {
 
         push.on('notification', function(data) {
             console.log('notification event');
-            navigator.notification.alert(
-                data.message,         // message
-                null,                 // callback
-                data.title,           // title
-                'Ok'                  // buttonName
-            );
+
+			var paramUrl = data.additionalData.url;
+			
+			var is_url = paramUrl.includes(mainDomain);
+			if(is_url){
+				goToUrl(paramUrl);
+				throw Error();
+			}else{
+				navigator.notification.alert(
+					data.message,         // message
+					null,                 // callback
+					data.title,           // title
+					'Ok'                  // buttonName
+				);
+			}
+			
+			//olah data disini
+			/*
+			data.url
+			if(data.additionalData.foreground)
+				window.alert(data.title+'\n'+data.message); //This is optional as I am trying to show 
+													 //Push message as an Alert when the app is in Foreground
+			//window.alert('insidePushDeviceReady');
+				   if(data.additionalData.coldstart){
+		   //redirecturl will be additional key in your payload to hold the relative url for redirection
+			window.open(data.additionalData.redirecturl); 
+			*/
+            
        });
     }
 };
+
+
